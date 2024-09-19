42c08f5b09f1644fb7b969eac266214952171c9c
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-09-19 10:48:43 +0530

    commit

--- workstation.sh
+++ workstation.sh
@@ -55,7 +55,7 @@ VALIDATE $? "kubectl installation"
 # VALIDATE $? "kubens installation"
 
 
-Helm
+# Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
